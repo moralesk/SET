61a4dc560018eecf7e4a6c00a241ea0546e3d25f
Kelly Morales <kmorales529@gmail.com>
2018-03-24 18:37:34 -0700

    Initial Commit

---
+++ Set/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Kelly Morales on 3/24/18.
+//  Copyright © 2018 Kelly Morales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
