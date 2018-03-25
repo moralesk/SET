ca0256b83b17ab1d593cc0d025a44e0de4183f84
Kelly Morales <kmorales529@gmail.com>
2018-03-25 15:10:23 -0700

    adds collectionView and collectionViewCell for selecting the number of players

--- Set/GameViewControllers/NumberOfPlayersViewController.swift
+++ Set/GameViewControllers/NumberOfPlayersViewController.swift
@@ -9,9 +9,11 @@
 import Foundation
 import UIKit
 
-class NumberOfPlayersViewController: UIViewController {
+class NumberOfPlayersViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
     let promptLabel = UILabel()
+    let maxNumberOfPlayers: Int = 4
+    var playerSelectionView: UICollectionView?
     var continueButton: MainSelectionButton?
 
     // MARK: Lifecycle Methods
@@ -23,6 +25,7 @@ class NumberOfPlayersViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         setupPromptLabel()
+        setupCollectionView()
     }
 
     // MARK: View Setup
@@ -43,5 +46,51 @@ class NumberOfPlayersViewController: UIViewController {
         let width = NSLayoutConstraint(item: promptLabel, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: promptLabel.superview, attribute: NSLayoutAttribute.width, multiplier: 1, constant: 0)
         let height = NSLayoutConstraint(item: promptLabel, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: promptLabel.superview, attribute: NSLayoutAttribute.height, multiplier: 0.1, constant: 0)
         NSLayoutConstraint.activate([centerX, top, width, height])
+    }
+    
+    private func setupCollectionView() {
+        if playerSelectionView?.superview == nil {
+            let layout = UICollectionViewFlowLayout()
+            playerSelectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+            guard let playerSelectionView = playerSelectionView else {
+                return
+            }
+            playerSelectionView.register(SelectionCollectionViewCell.self, forCellWithReuseIdentifier: "SelectionCell")
+            playerSelectionView.isScrollEnabled = false
+            playerSelectionView.backgroundColor = .white
+            playerSelectionView.delegate = self
+            playerSelectionView.dataSource = self
+            self.view.addSubview(playerSelectionView)
+            playerSelectionView.translatesAutoresizingMaskIntoConstraints = false
+            let centerX = NSLayoutConstraint(item: playerSelectionView, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: playerSelectionView.superview, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
+            let top = NSLayoutConstraint(item: playerSelectionView, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: promptLabel, attribute: NSLayoutAttribute.bottomMargin, multiplier: 1, constant: 40)
+            let width = NSLayoutConstraint(item: playerSelectionView, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: playerSelectionView.superview, attribute: NSLayoutAttribute.width, multiplier: 0.85, constant: 0)
+            let height = NSLayoutConstraint(item: playerSelectionView, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: playerSelectionView.superview, attribute: NSLayoutAttribute.height, multiplier: 0.1, constant: 0)
+            NSLayoutConstraint.activate([centerX, top, width, height])
+        }
+    }
+    
+    // MARK: UICollectionViewDataSource
+    // number of sections defaults to 1 without implementation
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return maxNumberOfPlayers
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SelectionCell", for: indexPath) as? SelectionCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.initialize(with: "\(indexPath.row + 1)")
+        return cell
+    }
+    
+    // MARK: UICollectionViewDelegateFlowLayout
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let height = self.view.bounds.height * 0.1
+        return CGSize(width: height, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 5.0
     }
 }
---
+++ Set/Views/SelectionCollectionViewCell.swift
@@ -0,0 +1,57 @@
+//
+//  SelectionCollectionViewCell.swift
+//  Set
+//
+//  Created by Kelly Morales on 3/25/18.
+//  Copyright © 2018 Kelly Morales. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/// UICollectionViewCell used in a UICollectionView displaying selectable options, such as the number of players for the game
+class SelectionCollectionViewCell: UICollectionViewCell {
+    private var labelText: String?
+    private let label = UILabel()
+
+    init(text: String) {
+        super.init(frame: .zero)
+        initialize(with: text)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    private func initialize() {
+        backgroundColor = .purple
+        layer.cornerRadius = 3.0
+        if label.superview == nil {
+            label.text = labelText
+            label.textColor = .white
+            label.font = UIFont.boldSystemFont(ofSize: 24)
+            label.textAlignment = .center
+            addSubview(label)
+        }
+        updateLabelConstraints()
+    }
+
+    func initialize(with text: String) {
+        initialize()
+        if label.superview != nil {
+            labelText = text
+            label.text = labelText
+        }
+    }
+
+    private func updateLabelConstraints() {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        let width = NSLayoutConstraint(item: label, attribute: .width, relatedBy: .equal, toItem: label.superview, attribute: .width, multiplier: 1, constant: 0)
+        let height = NSLayoutConstraint(item: label, attribute: .height, relatedBy: .equal, toItem: label.superview, attribute: .height, multiplier: 1, constant: 0)
+        NSLayoutConstraint.activate([width, height])
+    }
+}
