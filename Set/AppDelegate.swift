e8a0561337804bcdcdeccb2965c595784eb0b923
Kelly Morales <kmorales529@gmail.com>
2018-04-04 15:11:52 -0700

    adds to appDelegate to only support portrait orientation

--- Set/AppDelegate.swift
+++ Set/AppDelegate.swift
@@ -50,5 +50,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .portrait
+    }
+
 }
 
