28c7e02f7e9487c8ff8013149bfd12a3555d710a
Kelly Morales <kmorales529@gmail.com>
2018-12-31 18:23:41 -0500

    Adds GameBoardDataSourceProtocol

--- Set/Protocols/SETGameBoardDataSourceProtocol.swift
+++ Set/Protocols/SETGameBoardDataSourceProtocol.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+/// Protocol to abstract data source logic from the GameBoardViewController
+protocol SETGameBoardDataSourceProtocol: AnyObject {
+    /// A reference to an object that contains the gameBoard, generally a UIViewController
+    var gameBoardDelegate: SETGameBoardProtocol? { get set }
+
+    /// Creates UI elements for the possible number of players
+    func setupPlayerElements()
+
+    /**
+     Used to finish adding constraints to the game board since the game board is placed in view
+     after, and in respect to, the player elements
+     */
+    func completeGameBoardConstraints()
+
+    /// Determines if the given 3 cards are a SET, and updates the model and some UI accordingly
+    func processSet(_ cards: [Card])
+}
